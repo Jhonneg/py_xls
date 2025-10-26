b830640243ce7e3137fe0a912306ff064910a0a4
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-10-26 04:08:10 -0300

    full code

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import gspread
 gc = gspread.service_account("service-account-credentials.json")
 
 spreadsheet = gc.open("Python Gsheets")
+
+ws = spreadsheet.sheet1
+
+ws.update_acell("A1", "Hello from Python")
+
+new_spreadsheet = gc.create("Python Gsheets2")
+
+new_spreadsheet.share("jonee4339@gmail.com", perm_type="user", role="writer")
+
